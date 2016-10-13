c3cad119086f42c0e00a6d271b8679293675b6da
Tuan <nhutuan.ngo@gmail.com>
2016-10-13 18:14:59 +0700

    new code

--- src/pubsub.js
+++ src/pubsub.js
@@ -8,7 +8,9 @@ pub = sub = require('pubsub-js');
 // var redis = require("redis");
 // var sub = redis.createClient(), pub = redis.createClient();
 var Emitter = require('wildemitter');
-var EVENT =  require('./eventtype');
+
+//channel
+var VISITOR_JOIN = 'visitorjoin';
 
 function PubSub() {
 }
@@ -18,18 +20,41 @@ Emitter.mixin(PubSub);
 
 //public function
 PubSub.prototype.pubVisitorJoin = function(cid) {
-  var channel = EVENT.VISITOR_JOIN + ":" + cid;
+  var channel = VISITOR_JOIN + ":" + cid;
   pub.publish(channel, 'visitor is joined');
 }
 
 PubSub.prototype.subVisitorJoin = function(cid) {
   var self = this;
-  var channel = EVENT.VISITOR_JOIN + ":" + cid;
+  var channel = VISITOR_JOIN + ":" + cid;
 
   sub.subscribe(channel, function(msg, data) {
     console.log( 'operator join', msg, data );
     self.emit('VISITOR_JOIN');
   });
 }
+
+/*
+ * visitor channel
+ */
+PubSub.prototype.subVisitorChannel = function(vid) {
+  var channel = vid;
+  sub.subscribe(channel, function(msg, data) {
+    self.emit();
+  });
+}
+
+/*
+ * operators
+ * message to all online operators
+ */
+PubSub.prototype.subOperatorChannel = function (cid) {
+
+}
+
+PubSub.prototype.pubMessageChannel = function(message) {
+
+}
+
 
 module.exports = PubSub;
--- src/redisutil.js
+++ src/redisutil.js
@@ -1,3 +1,10 @@
+/*
+ * createdAt Oct 13
+ */
+
+//TODO add unique NODE-ID to key if use multiple signal nodes
+// e.g. var key = NODE-ID:VISITOR:VISITOR-ID
+
 var redis = require("redis"),
   client = redis.createClient();
 
@@ -5,7 +12,8 @@ function RedisUtil() {}
 
 //add
 RedisUtil.prototype.addVisitor = function(cid, vid) {
-  client.set(cid, vid);
+  var key = 'VISITOR' + cid;
+  client.set(key, vid);
 }
 
 RedisUtil.prototype.addVisitorPage = function(vid, page) {
@@ -13,9 +21,43 @@ RedisUtil.prototype.addVisitorPage = function(vid, page) {
 }
 
 RedisUtil.prototype.delVisitor = function(cid, vid) {
+  var key = 'VISITOR' + cid;
   //del vid in cid list
+  client.del(key, vid);
 
   //del vid page
+  key = 'VISITOR-PAGE' + vid;
+  client.del(key, vid);
+}
+
+/*
+ * return number of online operator
+ */
+RedisUtil.prototype.getOnlineOperator = function(cid) {
+  var key = 'OPERATOR' + cid;
+  var operators = client.get(key);
+  return operopers ? operators.length : 0;
+}
+
+RedisUtil.prototype.isFirstOperator = function(cid) {
+  return true;
+}
+
+/*
+ * get all visitor with visited pages
+ */
+RedisUtil.prototype.getVisitors = function(cid) {
+  var key = 'VISITOR' + cid;
+  var ret = client.get(key);
+
+  return ret ? ret : null;
+}
+
+/*
+ * add an operator with socket
+ */
+RedisUtil.prototype.addOperator = function(cid, oid, socket) {
+
 }
 
 module.exports = RedisUtil;
